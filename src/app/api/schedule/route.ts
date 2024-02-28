191b393665cdc38e5ffcc97441aaf31242b3c170
Bruno <brunosampayo1040@gmail.com>
2024-02-28 16:05:09 -0300

    feat: rout correct

--- src/app/api/schedule/route.ts
+++ src/app/api/schedule/route.ts
@@ -1,7 +1,14 @@
 export async function GET() {
-    return {
-        body: JSON.stringify({
-            message: "Hello World!"
-        })
-    }
+    return Response.json({ok:true})
+    
+        
+    
 }
+
+export async function POST(req:Request){
+    const data = await req.json()
+    console.log()
+    return Response.json(data)
+}
+
+
