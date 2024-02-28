0145421795b2df508fb54d0fade612d47d194d3d
Willian Donel <willianmadrugadonel@gmail.com>
2024-02-28 16:39:58 -0300

    comitando o header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html className="flex flex-col-reverse" lang="en">
       <Header/>
       <body className={inter.className}>{children}</body>
     </html>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
     return(
-        <div className=" flex items-center  h-28 w-full bg-blue-500 opacity-80 rounded-b-3xl ">
+        <div className=" flex items-center  h-28 w-full bg-blue-500 opacity-80 rounded-b-3xl z-10 ">
             <h1 className="font-serif mx-auto text-yellow-200  text-3xl">Espaço Alegria</h1>
         </div>
     )
