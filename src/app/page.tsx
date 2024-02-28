ba605166d75f18148992f661fd7a139c40d88958
Willian Donel <willianmadrugadonel@gmail.com>
2024-02-28 15:07:42 -0300

    adicioanado a biblioteca do shadcn e component header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+
 import Image from "next/image";
+import {Header} from '@/components/Header';
+import { Calendar } from "@/components/ui/calendar"
+import { useState} from 'react'
+
 
 export default function Home() {
+  const [date, setDate] = useState<Date | undefined>(new Date())
   return (
-    <h1>teste</h1>
+    <div className="flex flex-col items-center justify-between">
+      <Header/>
+
+    
+
+  <Calendar
+    mode="single"
+    selected={date}
+    onSelect={setDate}
+    className="rounded-md border"
+  />
+
+      
+      
+    </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+    return(
+        <div className=" flex items-center  h-28 w-full bg-blue-500 opacity-80 rounded-b-3xl">
+            <h1 className="font-serif mx-auto text-yellow-200  text-3xl">Espaço Alegria</h1>
+        </div>
+    )
+}
