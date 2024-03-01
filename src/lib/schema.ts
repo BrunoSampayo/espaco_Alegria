d92c342a584acc163e667e0fa3d43feec62786a5
Bruno <brunosampayo1040@gmail.com>
2024-03-01 11:15:43 -0300

    feat: schema correction

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -1,22 +1,22 @@
 import z from 'zod'
 
 export const scheduleSchema = z.object({
-    nomeCliente: z.string(),
-    numeroCelular: z.string(),
+    nome_cliente: z.string(),
+    numero_celular: z.string(),
     data: z.string().transform((str) => new Date(str)),
-    horaInicio: z.string(),
-    horaFim: z.string(),
+    hora_inicio: z.string(),
+    hora_fim: z.string(),
     feriado: z.boolean(),
-    touroMecanico: z.string().transform((str) => parseInt(str)),//hora
+    touro_mecanico: z.string().transform((str) => parseInt(str)),//hora
     fotos: z.string().transform((str) => parseInt(str)),//hora
     garcom: z.string().transform((str) => parseInt(str)),
     dj: z.string().transform((str) => parseInt(str)),
     climatizacao: z.string().transform((str) => parseInt(str)),
     churrasqueira: z.string().transform((str) => parseInt(str)),
     telao: z.string().transform((str) => parseInt(str)),
-    taxaLuz: z.string().transform((str) => parseInt(str)),
-    valorBuffet: z.string().transform((str) => parseFloat(str)),
+    taxa_luz: z.string().transform((str) => parseInt(str)),
+    valor_buffet: z.string().transform((str) => parseInt(str)),
     observacao: z.string(),
-    valorSugerido: z.number(),
-    valorCobrado: z.number()
+    valor_sugerido: z.string().transform((str) => parseFloat(str)),
+    valor_cobrado: z.string().transform((str) => parseFloat(str))
 })
