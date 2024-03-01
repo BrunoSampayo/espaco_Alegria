5cd1f505eb20492709d32c770749b4723543274b
Bruno <brunosampayo1040@gmail.com>
2024-03-01 11:00:26 -0300

    feat: function from get schedule and post bug correction

--- src/app/api/schedule/route.ts
+++ src/app/api/schedule/route.ts
@@ -1,4 +1,4 @@
-import { Prisma } from '@/lib/prisma'
+import  prisma  from '@/lib/prisma'
 import { scheduleSchema } from '@/lib/schema'
 import { NextRequest } from 'next/server';
 import 'dayjs'
@@ -8,18 +8,25 @@ export async function GET(req: NextRequest) {
     //data format yyyy-mm-dd
 
 
-    const startOfMonth = dayjs(data).startOf('month').add(-1, 'day').toDate();
-    const endOfMonth = dayjs(data).endOf('month').add(-1, 'day').toDate();
-    const schedules = await Prisma.schedule.findMany({
+    const startOfMonth = dayjs(data).startOf('month').add(-1, 'day').toDate().setUTCHours(0,0,0,0);
+    const endOfMonth = dayjs(data).endOf('month').endOf('hour').add(-1, 'day').toDate().setUTCHours(23,59,59,999);
+    console.log(new Date(startOfMonth))
+    console.log(new Date(endOfMonth))
+   
+    const schedules = await prisma.schedule.findMany({
         where: {
             data: {
-                lte: endOfMonth,
-                gte: startOfMonth
+                gte: new Date(startOfMonth),
+                lte: new Date(endOfMonth)
             }
         }
     })
-
-    Response.json(schedules)
+    
+    
+    if(schedules.length===0){
+        return Response.json({ error: "Nenhum agendamento encontrado" })
+    }
+    return  Response.json(schedules)
 }
 
 
@@ -28,26 +35,26 @@ export async function POST(req: Request) {
     console.log(new Date().toISOString())
     try {
         const valide = scheduleSchema.parse(data)
-        const newSchedule = await Prisma.schedule.create({
+        const newSchedule = await prisma.schedule.create({
             data: {
-                nome_cliente: valide.nome_cliente,
-                numero_celular: valide.numero_celular,
+                nome_cliente: valide.nomeCliente,
+                numero_celular: valide.numeroCelular,
                 data: valide.data,
-                hora_inicio: valide.hora_inicio,
-                hora_fim: valide.hora_fim,
+                hora_inicio: valide.horaInicio,
+                hora_fim: valide.horaFim,
                 feriado: valide.feriado,
-                touro_mecanico: valide.touro_mecanico,
+                touro_mecanico: valide.touroMecanico,
                 fotos: valide.fotos,
                 garcom: valide.garcom,
                 dj: valide.dj,
                 climatizacao: valide.climatizacao,
                 churrasqueira: valide.churrasqueira,
                 telao: valide.telao,
-                taxa_luz: valide.taxa_luz,
-                valor_buffet: valide.valor_buffet,
+                taxa_luz: valide.taxaLuz,
+                valor_buffet: valide.valorBuffet,
                 observacao: valide.observacao,
-                valor_sugerido: valide.valor_sugerido,
-                valor_cobrado: valide.valor_cobrado
+                valor_sugerido: valide.valorSugerido,
+                valor_cobrado: valide.valorCobrado
 
             }
 
@@ -56,10 +63,24 @@ export async function POST(req: Request) {
 
     }
     catch (err) {
-        return Response.json(err)
+        const error:any=err
+        return Response.json(error.message)
     }
 
 
 }
 
 
+export async function PUT(req: NextRequest) {
+    const dados = await req.json()
+    const id = req.nextUrl.searchParams.get("id") as string;
+    try {
+        const valide = scheduleSchema.parse(dados)
+        const editedSchedule = await prisma.schedule.update({
+            where: { id: id }, data: valide
+        })
+        return Response.json({ message: `Agendamento do ${editedSchedule.nome_cliente} alterado com Sucesso` })
+    } catch (err) {
+        return Response.json("error" + err)
+    }
+}
