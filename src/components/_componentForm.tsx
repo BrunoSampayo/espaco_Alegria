ba77266abf1028c2f6860751ba392b0712ece267
Willian Donel <willianmadrugadonel@gmail.com>
2024-03-05 14:48:35 -0300

    frontend

--- src/components/_componentForm.tsx
+++ src/components/_componentForm.tsx
@@ -9,7 +9,7 @@
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-import { Checkbox } from "@/components/ui/checkbox"
+
 import {zodResolver} from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { valideFormSchema } from "@/lib/schema"
